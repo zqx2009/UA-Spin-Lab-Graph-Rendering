ea4f7056235660ba306d73929b3d19e01eb71eaf
buttonbugs <miller.chen2006@outlook.com>
2026-03-26 12:53:31 -0700

    Finish TMR graph

---
+++ Hc_Temp.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+save_graph_file_path = "Hc_Temp.png"
+data_file_path = "Hc_Temp.txt"
+separator = "\t"
+
+# Data points
+
+def read_data():
+    with open(data_file_path, "r") as data_file:
+        data_str = data_file.read()
+
+    xpoints = []
+    ypoints = []
+
+    for data_row_str in data_str.split("\n")[1:]:
+        data_row = data_row_str.split(separator)
+        xpoints.append(float(data_row[2][:-1]))     # [:-1] to remove 'K'
+        ypoints.append(abs(float(data_row[0]) + float(data_row[1])) / 2)    # take the absolute value of the exchange bias
+
+    return np.array(xpoints), np.array(ypoints)
+
+
+def plot_data(xpoints, ypoints):
+
+    # Plot the points and connect with a line
+    plt.plot(xpoints, ypoints, color= "black")
+
+    # Add labels and a title
+    plt.xlabel("Temperature (K)")
+    plt.ylabel("Exchange bias")
+    # plt.yscale('log') # Set the y-axis to logarithmic scale
+    plt.title("Exchange bias - temperature")
+
+    plt.savefig(save_graph_file_path, dpi=300, transparent=False, bbox_inches='tight')
+
+    # Display the plot
+    plt.show()
+
+if __name__ == "__main__":
+    xpoints, ypoints = read_data()
+    plot_data(xpoints, ypoints)
---
+++ TMR.py
@@ -0,0 +1,61 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+data_file_path = "TMR.txt"
+separator = "\t"
+
+# Data points
+
+def read_data():
+    with open(data_file_path, "r") as data_file:
+        data_str = data_file.read()
+
+    xpoints = []
+    ypoints = []
+
+    for data_row_str in data_str.split("\n")[1:]:
+        data_row = data_row_str.split(separator)
+        if len(data_row) == 3:
+            xpoints.append(float(data_row[0]))
+            ypoints.append(abs(float(data_row[1])))
+
+    return np.array(xpoints), np.array(ypoints)
+
+
+def plot_R(xpoints, ypoints):
+
+    # Plot the points and connect with a line
+    plt.plot(xpoints, ypoints, color= "black")
+
+    # Add labels and a title
+    plt.xlabel("Applied Field (Oe)")
+    plt.ylabel("Resistance (Ω)")
+    # plt.yscale('log') # Set the y-axis to logarithmic scale
+    plt.title("Resistance - Applied Field")
+
+    plt.savefig("TMR_R.png", dpi=300, transparent=False, bbox_inches='tight')
+
+    # Display the plot
+    plt.show()
+
+def plot_TMR(xpoints, ypoints):
+
+    # Plot the points and connect with a line
+    plt.plot(xpoints, ypoints, color= "black")
+
+    # Add labels and a title
+    plt.xlabel("Applied Field (Oe)")
+    plt.ylabel("TMR (%)")
+    # plt.yscale('log') # Set the y-axis to logarithmic scale
+    plt.title("TMR")
+
+    plt.savefig("TMR_TMR.png", dpi=300, transparent=False, bbox_inches='tight')
+
+    # Display the plot
+    plt.show()
+
+if __name__ == "__main__":
+    field, resistance = read_data()
+    plot_R(field, resistance)
+    R_p = np.min(resistance)
+    plot_TMR(field, (resistance-R_p)/R_p*100)
