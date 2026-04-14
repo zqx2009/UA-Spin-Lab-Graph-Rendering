ed4654a112aec3e4eb3c53c298c69a8ee3d5db74
buttonbugs <miller.chen2006@outlook.com>
2026-04-14 16:57:40 -0700

    Remove jumping

--- field_cooling.py
+++ field_cooling.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
-plot_debug = False
+plot_debug = True
 remove_wafer_background = True
 y_scaling_degree = -7
 
@@ -10,11 +10,14 @@ sample_data_path = "field_cooling_sample.dat"
 wafer_data_path = "field_cooling_wafer.dat"
 
 output_path = "field_cooling.png"
+background_field_output_path = "field_cooling_background.png"
+background_0_field_output_path = "field_0_cooling_background.png"
+sample_field_output_path = "field_cooling_sample.png"
+sample_0_field_output_path = "field_0_cooling_sample.png"
 
 def read_csv_to_json(csv_file_path):
     df = pd.read_csv(csv_file_path, keep_default_na=False)      # keep_default_na = True: return NaN if empty; keep_default_na = False: return '' if empty
     return df.to_dict(orient='records')
-
 
 """ read data """
 wafer_data = read_csv_to_json(wafer_data_path)
@@ -24,6 +27,15 @@ sample_data = read_csv_to_json(sample_data_path)
 """ wafer """
 # filter data
 wafer_data_field = [d for d in wafer_data if d["Magnetic Field (Oe)"] > 4900]
+
+jump_correction = []
+for data_point in wafer_data_field:
+    if data_point["Temperature (K)"] >= 244.0:
+        data_point["Moment (emu)"] -= 4E-7
+    jump_correction.append(data_point)
+
+wafer_data_field = jump_correction[:]
+
 wafer_data_temp_field = np.array([d["Temperature (K)"] for d in wafer_data_field])
 wafer_data_moment_field = np.array([d["Moment (emu)"] for d in wafer_data_field])
 
@@ -43,6 +55,7 @@ if plot_debug:
     plt.xlabel('Temperature (K)')
     plt.ylabel('Moment (emu)')
     plt.legend()
+    plt.savefig(background_field_output_path, dpi=300, transparent=False, bbox_inches='tight')
     plt.show()
 
 """ sample """
@@ -51,15 +64,16 @@ sample_data_field = [d for d in sample_data if d["Magnetic Field (Oe)"] > 4900]
 sample_data_temp_field = np.array([d["Temperature (K)"] for d in sample_data_field])
 sample_data_moment_field = np.array([d["Moment (emu)"] for d in sample_data_field])
 
-# plot graph
-if remove_wafer_background:
-    sample_data_moment_field -= wafer_field_cooling_trend_func(sample_data_temp_field)
-
 if plot_debug:
     plt.scatter(sample_data_temp_field, sample_data_moment_field, label="NiO Sample Field Cooling")
     plt.xlabel('Temperature (K)')
     plt.ylabel('Moment (emu)')
+    plt.savefig(sample_field_output_path, dpi=300, transparent=False, bbox_inches='tight')
     plt.show()
+
+# plot graph
+if remove_wafer_background:
+    sample_data_moment_field -= wafer_field_cooling_trend_func(sample_data_temp_field)
 
 """ 0 field cooling ============================================ """
 """ wafer """
@@ -84,6 +98,7 @@ if plot_debug:
     plt.xlabel('Temperature (K)')
     plt.ylabel('Moment (emu)')
     plt.legend()
+    plt.savefig(background_0_field_output_path, dpi=300, transparent=False, bbox_inches='tight')
     plt.show()
 
 """ sample """
@@ -92,19 +107,20 @@ sample_data_0_field = sample_data[1791:2928]
 sample_data_temp_0_field = np.array([d["Temperature (K)"] for d in sample_data_0_field])
 sample_data_moment_0_field = np.array([d["Moment (emu)"] for d in sample_data_0_field])
 
-# plot graph
-if remove_wafer_background:
-    sample_data_moment_0_field -= wafer_0_field_cooling_trend_func(sample_data_temp_0_field)
-
 if plot_debug:
     plt.scatter(sample_data_temp_0_field, sample_data_moment_0_field, label="NiO Sample 0 Field Cooling")
     plt.xlabel('Temperature (K)')
     plt.ylabel('Moment (emu)')
+    plt.savefig(sample_0_field_output_path, dpi=300, transparent=False, bbox_inches='tight')
     plt.show()
 
+# plot graph
+if remove_wafer_background:
+    sample_data_moment_0_field -= wafer_0_field_cooling_trend_func(sample_data_temp_0_field)
+
 """ field cooling nad 0 field cooling in one graph """
-plt.scatter(sample_data_temp_field, sample_data_moment_field * 10**(-y_scaling_degree), label="NiO Sample Field Cooling")
 plt.scatter(sample_data_temp_0_field, sample_data_moment_0_field * 10**(-y_scaling_degree), label="NiO Sample 0 Field Cooling")
+plt.scatter(sample_data_temp_field, sample_data_moment_field * 10**(-y_scaling_degree), label="NiO Sample Field Cooling")
 plt.xlabel('Temperature (K)')
 plt.ylabel(f'Moment ($10^{{{y_scaling_degree}}}$ emu)')
 plt.grid()
