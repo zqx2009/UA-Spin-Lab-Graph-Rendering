ea960057dbbc2c5c240fabf97270a3269b4b9757
buttonbugs <114892917+buttonbugs@users.noreply.github.com>
2026-07-16 19:32:15 -0700

    Undo changes in 10K_hysteresis_with_inset.py

--- 10K_hysteresis_with_inset.py
+++ 10K_hysteresis_with_inset.py
@@ -29,43 +29,7 @@ def read_csv_to_json(csv_file_path):
     df = pd.read_csv(csv_file_path, keep_default_na=False)      # keep_default_na = True: return NaN if empty; keep_default_na = False: return '' if empty
     return df.to_dict(orient='records')
 
-
 if __name__ == "__main__":
-    """ 10K Hysteresis """
-    fig, ax = plt.subplots(figsize=(6, 4))
-
-    hysteresis_data = read_csv_to_json(hysteresis_data_path)
-
-    hysteresis_data_low_temp = hysteresis_data[3732:10694]
-    hysteresis_data_low_temp = [d for d in hysteresis_data_low_temp if d["Magnetic Field (Oe)"] > -2000 and d["Magnetic Field (Oe)"] < 1000]
-
-    H = np.array([d["Magnetic Field (Oe)"] for d in hysteresis_data_low_temp])
-    Moment = np.array([d["Moment (emu)"] for d in hysteresis_data_low_temp])
-    Magnetization = Moment / sample_volume
-
-    ax.plot(H, Magnetization, color="black", marker='s')
-    ax.set_xlabel('Magnetic Field (Oe)')
-    ax.set_ylabel('Magnetization (emu/cm$^3$)')
-    ax.grid(linewidth = 0.3)
-    ax.text(0.85, 0.1, "10K", transform=ax.transAxes, fontsize=40, ha='center', va='center')
-
-    """ Inset """
-    ax_inset = ax.inset_axes([0.16, 0.58, 0.45, 0.4])
-    xpoints, ypoints = read_data("Hc_Temp_1.txt")
-    ax_inset.plot(xpoints, ypoints, color = "red", marker='s', markersize=3, linewidth = 1, label="($+$) FC")
-
-    xpoints, ypoints = read_data("Hc_Temp_2.txt")
-    ax_inset.plot(xpoints, ypoints, color = "blue", marker='s', markersize=3, linewidth = 1, label="($-$) FC")
-    ax_inset.legend()      # Show legend
-    ax_inset.set_xlabel("Temperature (K)")
-    ax_inset.set_ylabel("Exchange bias (Oe)")
-    ax_inset.set_ylim(-400, 400)
-    ax_inset.grid(linewidth = 0.2)
-    plt.savefig(save_graph_file_path, dpi=300, transparent=False, bbox_inches='tight')
-    plt.show()
-
-if __name__ == "__main__":
-    plt.clf()
     """ 10K Hysteresis """
     fig, ax = plt.subplots(figsize=(6, 4))
 
